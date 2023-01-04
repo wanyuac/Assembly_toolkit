2b586d35748fa86e7cd331d273f2eeaf3adb2bc7
wanyuac <wanyuac@126.com>
2023-01-04 10:26:32 +0000

    update

--- run_flye.sh
+++ run_flye.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 # Run Flye assembler for long-read assemblies
-# run_flye.sh [isolate name] [input read file] [number of polishing rounds] [Filename extension of the input read file (fastq/fastq.gz/etc)]
+# run_flye.sh [input read file] [number of polishing rounds] [Filename extension of the input read file (fastq/fastq.gz/etc)] [possible genome size] [number of threads]
 # Change the number of threads for your computer.
-# Copyright (C) 2022 Yu Wan <wanyuac@126.com>
+# Copyright (C) 2022-2023 Yu Wan <wanyuac@126.com>
 # Licensed under the GNU General Public Licence version 3 (GPLv3) <https://www.gnu.org/licenses/>.
-# First version: 18 Dec 2022; latest update: 18 Dec 2022
+# First version: 18 Dec 2022; latest update: 3 Jan 2023
 
 # Read parameters ###############
-i="$1"  # Isolate name
-r="$2"  # Input read file (filename extensions: fastq, fastq.gz, etc)
-p="$3"  # Number of polishing rounds
-e="$4"  # Filename extension of the input read file (fastq/fastq.gz/etc)
-g="$5"  # Genome size (for instance, 5m)
+r="$1"  # Input read file (filename extensions: fastq, fastq.gz, etc)
+p="$2"  # Number of polishing rounds
+e="$3"  # Filename extension of the input read file (fastq/fastq.gz/etc)
+g="$4"  # Genome size (for instance, 5m)
+t="$5"  # Number of threads
 
 # Generate a genome assembly from long reads ###############
 f=$(basename $r ".$e")
 pre="${f}_flye_p${p}"
 v=`flye --version`
-echo "Assembling long reads $r using Flye v$v (polish: ${p})"
+if [ -z "$t" ]; then
+t=1
+fi
+echo "Assembling long reads $r using Flye v$v (polish: ${p}) with $t threads"
 mkdir ./tmp
-flye --nano-raw $r --threads 4 --out-dir ./tmp --iterations $p --genome-size $g --scaffold  # --subassemblies is incompatible with --nano-raw or --nano-corr
+flye --nano-raw $r --threads $t --out-dir ./tmp --iterations $p --genome-size $g --scaffold  # --subassemblies is incompatible with --nano-raw or --nano-corr
 mv ./tmp/assembly.fasta ${pre}.fasta
 mv ./tmp/assembly_graph.gfa ${pre}.gfa
 mv ./tmp/assembly_info.txt ${pre}.txt
--- run_raven.sh
+++ run_raven.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 # Run Raven assembler for long-read assemblies
-# run_raven.sh [isolate name] [input read file] [k-mer size] [window size] [number of polishing rounds] [Filename extension of the input read file (fastq/fastq.gz/etc)] [number of threads]
+# run_raven.sh [input read file] [Filename extension of the input read file (fastq/fastq.gz/etc)] [k-mer size] [window size] [number of polishing rounds] [number of threads]
 # Change the number of threads for your computer.
-# Copyright (C) 2022 Yu Wan <wanyuac@126.com>
+# Copyright (C) 2022-2023 Yu Wan <wanyuac@126.com>
 # Licensed under the GNU General Public Licence version 3 (GPLv3) <https://www.gnu.org/licenses/>.
-# First version: 18 Dec 2022; latest update: 24 Dec 2022
+# First version: 18 Dec 2022; latest update: 3 Jan 2023
 
 # Read parameters ###############
-i="$1"  # Isolate name
-r="$2"  # Input read file
+r="$1"  # Input read file
+e="$2"  # Filename extension of the input read file, without the leading '.' sign (namely, fastq/fastq.gz/etc)
 k="$3"  # K-mer size
 w="$4"  # Window length
 p="$5"  # Polish rounds
-e="$6"  # Filename extension of the input read file (without '.'; For example, 'fastq')
-t="$7"  # Number of threads
+t="$6"  # Number of threads
 
 # Run Raven assembler ###############
-f=$(basename $r ".$e")
+f=$(basename "$r" ".$e")
 pre="${f}_raven_k${k}w${w}p${p}"
 v=`raven --version`
 if [ -z "$t" ]; then
-    echo 'Set the number of threads to 2'
-    t=2
+    echo 'Set the number of threads to 1'
+    t=1
 fi
-echo "Assembling long reads $r using Raven $v (k=${k}, w=${w}; polish $p times)"
+echo "Assembling long reads from $r using Raven $v (k=${k}, w=${w}; polish=${p}; threads=${t})"
 raven --threads $t --polishing-rounds $p --kmer-len $k --window-len $w --disable-checkpoints --graphical-fragment-assembly ${pre}.gfa $r > ${pre}.fasta
