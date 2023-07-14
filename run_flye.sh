01d25a756a5428ccf5db3f65b14919290297f0ca
Yu Wan <wanyuac@126.com>
2023-07-15 00:17:37 +0100

    Update run_flye.sh

--- run_flye.sh
+++ run_flye.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Run Flye assembler for long-read assemblies
-# run_flye.sh [input read file] [Filename extension of the input read file (fastq/fastq.gz/etc)] [probable genome size] [number of polishing rounds] [number of threads]
+# Run Flye to assemble (Oxford Nanopore Technologies) ONT reads
+# run_flye.sh [input read file] [Filename extension of the input read file (fastq/fastq.gz/etc)] [probable genome size] [number of polishing rounds] [number of threads] [error level of reads]
 # Change the number of threads for your computer.
 # Copyright (C) 2022-2023 Yu Wan <wanyuac@126.com>
 # Licensed under the GNU General Public Licence version 3 (GPLv3) <https://www.gnu.org/licenses/>.
-# First version: 18 Dec 2022; latest update: 3 June 2023
+# First version: 18 Dec 2022; latest update: 15 July 2023
 
 # Read parameters ###############
 r="$1"  # Input read file (filename extensions: fastq, fastq.gz, etc)
@@ -12,16 +12,27 @@ e="$2"  # Filename extension of the input read file (fastq/fastq.gz/etc)
 g="$3"  # Probable genome size (for instance, 5m)
 p="$4"  # Number of polishing rounds
 t="$5"  # Number of threads
+m="$6"  # Mode of Nanopore reads: raw (expected error rate: <15%) or hq (<5%)
 
 # Generate a genome assembly from long reads ###############
 f=$(basename $r ".$e")
 pre="${f}_flye_p${p}"
 v=`flye --version`
-if [ -z "$t" ]; then t=1; fi
-echo "Assembling long reads $r using Flye v$v (polish: ${p}) with $t threads"
+if [ -z "$t" ]; then
+    t=1;
+fi
+if [ -z "$m" ]; then
+    m="raw"
+fi
 mkdir ./tmp
 
-flye --nano-raw $r --threads $t --out-dir ./tmp --iterations $p --genome-size $g  # --subassemblies is incompatible with --nano-raw or --nano-corr
+if [ "$m" == "hq" ]; then
+    echo "Assembling high-quality (error rate <5%) ONT reads $r using Flye v$v (polish: ${p}) with $t threads"
+    flye --nano-hq $r --threads $t --out-dir ./tmp --iterations $p --genome-size $g  # --subassemblies is incompatible with --nano-raw or --nano-corr
+else
+    echo "Assembling regular (error rate <15%) ONT reads $r using Flye v$v (polish: ${p}) with $t threads"
+    flye --nano-raw $r --threads $t --out-dir ./tmp --iterations $p --genome-size $g
+fi
 
 mv ./tmp/assembly.fasta ${pre}.fasta
 mv ./tmp/assembly_graph.gfa ${pre}.gfa
