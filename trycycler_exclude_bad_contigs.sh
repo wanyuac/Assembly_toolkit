154e2c54589fca05ac109f2b507b1a73241a94d3
Yu Wan <wanyuac@126.com>
2024-11-12 11:33:36 +0000

    Update trycycler_exclude_bad_contigs.sh

--- trycycler_exclude_bad_contigs.sh
+++ trycycler_exclude_bad_contigs.sh
@@ -2,24 +2,27 @@
 # Rename bad contigs to exclude them before running 'trycycler reconcile'.
 # Copyright (C) 2024 Yu Wan <wanyuac@gmail.com>
 # Licensed under the GNU General Public Licence version 3 (GPLv3) <https://www.gnu.org/licenses/>.
-# First version: 6 November 2024; last update: 6 November 2024
+# First version: 6 November 2024; last update: 12 November 2024
 
 # Help information ###############
 display_usage() {
     echo "
-    This script renames bad contigs to exclude them from inputs of command 'trycycler reconcile'.
+    This script renames bad contigs to exclude them from inputs of command \'trycycler reconcile\'.
 
     Parameters:
-      -i=*: Input directory - the output directory of 'trcycyler cluster'
-      -t=*: A two-column TSV file listing cluster indices (e.g., 001, 002) and filenames without .fasta (e.g., L_utg000002c)
+      -i=*: Input directory - the output directory of \'trcycyler cluster\'
+      -t=*: A two-column TSV file listing cluster names (e.g., cluster_001) and filenames without .fasta (e.g., L_utg000002c)
     
     Example useage:
-      /usr/local/bin/Assembly_toolkit/trycycler_exclude_bad_contigs.sh -i=2_clusters -t=contigs_to_exclude.tsv
+      ~/bin/Assembly_toolkit/trycycler_exclude_bad_contigs.sh -i=2_clusters -t=contigs_to_exclude.tsv
+    
+    Format of the TSV file:
+    [cluster name]\t[comma-delimited contig names]
     "
 }
 
 # Main ###############
-if [ -z $1 ]
+if [ -z "$1" ]
 then
     display_usage
     exit
@@ -43,13 +46,17 @@ while read line
 do
     if [ ! -z "$line" ]
     then
-        IFS=$'\t' read cluster_index contig <<< "$line"
-        fasta_source="$dir_in/cluster_${cluster_index}/1_contigs/${contig}.fasta"
-        fasta_target="${fasta_source}.bad"
-        if [ -f "$fasta_source" ]
-        then
-            echo "$fasta_source -> $fasta_target"
-            mv $fasta_source $fasta_target
-        fi
+        IFS=$'\t' read cluster_name contig_list <<< "$line"
+        IFS=',' read -r -a contigs <<< "$contig_list"
+        for contig in "${contigs[@]}"
+        do
+            fasta_source="$dir_in/${cluster_name}/1_contigs/${contig}.fasta"
+            fasta_target="${fasta_source}.bad"
+            if [ -f "$fasta_source" ]
+            then
+                echo "Rename $fasta_source as $fasta_target"
+                mv "$fasta_source" "$fasta_target"
+            fi
+        done
     fi
 done < "$tsv"
