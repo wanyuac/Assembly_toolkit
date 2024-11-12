a77b7ec9e54e63710d4191972a9ff3aa6ee7495d
Wan <yuwan@liverpool.ac.uk>
2024-11-12 16:06:34 +0000

    Update trycycler_generate_consensus.sh

--- trycycler_generate_consensus.sh
+++ trycycler_generate_consensus.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Run the last three steps of Trycycler: msa, partition, and consensus.
-#
 # Copyright (C) 2024 Yu Wan <wanyuac@gmail.com>
 # Licensed under the GNU General Public Licence version 3 (GPLv3) <https://www.gnu.org/licenses/>.
 # First version: 12 November 2024; last update: 12 November 2024
@@ -8,16 +7,15 @@
 # Help information ###############
 display_usage() {
     echo "
-    This script renames bad contigs to exclude them from inputs of command 'trycycler reconcile'.
+    This script carries out the last three steps of Trycycler: msa, partition, and consensus.
 
     Parameters:
       -r=*: The input long reads
       -d=*: The input directory or the output directory of 'trcycyler cluster', which serves as the input directory for 'trycycler reconcile' (default: 2_clusters)
-      -s=*: The common suffix to add to log files (default: none)
       -t=*: Number of threads (default: 1)
     
     Example useage:
-      ~/bin/Assembly_toolkit/trycycler_generate_consensus.sh -i=2_clusters -n=3 -s=round1 -t=16 -p
+      ~/bin/Assembly_toolkit/trycycler_generate_consensus.sh -r=sample1.fastq.gz -d=2_clusters -t=16 >trycycler_generate_consensus.log 2>&1
 
     Dependencies: trycycler, perl.
     "
@@ -35,47 +33,66 @@ clean_log() {
 }
 
 # Main procedure ###############
-# Set default values
+# Set default values ===============
 dir_in='2_clusters'
 t=1
-draw_plot=false
 
-# Read parameters
+# Read parameters ===============
 for arg in "$@"
 do
     case $arg in
         -r=*)
-        reads="${arg#*=}"
+        long_reads="${arg#*=}"
         ;;
         -d=*)
         dir_in="${arg#*=}"
         ;;
-        -s=*)
-        s="${arg#*=}"
-        ;;
         -t=*)
         t="${arg#*=}"
-        if [ $t -lt 1 ]
-        then
-            echo "Error: the number of threads is negative. Reset to 1."
-            t=1
-        fi
         ;;
         *)
         ;;
     esac
 done
 
-# Multi-sequence aligment
-for i in `seq 1 3`; do trycycler msa --threads 16 --cluster_dir "2_clusters_min${min_depth}x_round2/cluster_00${i}"; done
+# Sanity check ===============
+if [ -f "$long_reads" ]
+then
+    echo "[$(date)] Input read file: $long_reads"
+else
+    echo "[$(date)] Error: read file $long_reads was not found."
+    exit 1
+fi
 
-# Partitioning reads
-trycycler partition --threads 16 --reads $long_reads --cluster_dirs "2_clusters_min${min_depth}x_round2"/cluster_00?
+if [ $t -lt 1 ]
+then
+    echo "[$(date)] Error: the number of threads is negative. Reset to 1."
+    t=1
+fi
+echo "[$(date)] Number of threads: $t"
 
-# Generating a consensus
-for i in `seq 1 3`
+# Multi-sequence aligment ===============
+cluster_dirs=( $(ls -1 -d "$dir_in"/cluster_*/) )
+echo "[$(date)] Found ${#cluster_dirs[@]} clusters in $dir_in"
+echo "[$(date)] Conduct multi-sequence alignment"
+
+for d in "${cluster_dirs[@]}"
 do
-    trycycler consensus --threads 16 --cluster_dir "2_clusters_min${min_depth}x_round2/cluster_00$i" >> trycycler_consensus.txt 2>&1
+    trycycler msa --threads "$t" --cluster_dir "${d%/}"
 done
 
+# Partitioning reads
+echo "[$(date)] Partition long reads"
+
+trycycler partition --threads "$t" --reads "$long_reads" --cluster_dirs "$dir_in"/cluster_00?
+
+# Generating a consensus ===============
+echo "[$(date)] Produce consensus sequences"
+
+for d in "${cluster_dirs[@]}"
+do
+    trycycler consensus --threads "$t" --cluster_dir "${d%/}" >> trycycler_consensus.txt 2>&1
+done
+
+# Finish up ===============
 clean_log trycycler_consensus.txt > trycycler_consensus.log && rm trycycler_consensus.txt
